c4c8fb0c1118bb3af189fd959df50b9b9d3a3473
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-22 00:02:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,7 @@ def monopoly_with_third_tier
 
 monopoly_with_third_tier = {
     :railroads => {
-    :rent_in_dollars => 22,
-    :one_piece_owned => {},
+    :rent_in_dollars => {   :one_piece_owned => { }  },
     :pieces => 4, 
     :rent_in_dollars => {},
     :names => ""
