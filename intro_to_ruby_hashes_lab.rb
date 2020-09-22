89f2b4f29026aae7680cf4dda481ccf1acbecd3b
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-22 00:17:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,6 +36,7 @@ monopoly_with_third_tier = {
 end
 
 def monopoly_with_fourth_tier
+
 monopoly_with_fourth_tier = {
     :railroads => {
     
@@ -46,7 +47,7 @@ monopoly_with_fourth_tier = {
                         },
     :pieces => 4,
     
-    :names => {:reading_railroad => {},
+    :names => {:reading_railroad => {:mortgage_value => 100},
                :pennsylvania_railroad => {},
                :b_and_o_railroad => {},
                :shortline_railroad => {}
